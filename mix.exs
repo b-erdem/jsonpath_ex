0fcfdce96bacefe3690c2bd12d3a858a3ce9e266
Baris Erdem <baris@erdem.dev>
2024-12-03 12:59:43 +0100

    Add metadata.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule JsonpathEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "JSONPathEx",
+      description: description(),
+      source_url: "https://github.com/b-erdem/jsonpath_ex",
+      homepage_url: "https://hex.pm/packages/jsonpath_ex",
+      docs: &docs/0
     ]
   end
 
@@ -18,12 +24,33 @@ defmodule JsonpathEx.MixProject do
     ]
   end
 
+  defp description do
+    "A powerful Elixir library for parsing and evaluating JSONPath expressions."
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_parsec, "~> 1.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:nimble_parsec, "~> 1.0"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "JSONPathEx",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Baris Erdem baris@erdem.dev"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/b-erdem/jsonpath_ex",
+        "Hex" => "https://hex.pm/packages/jsonpath_ex"
+      }
     ]
   end
 end
